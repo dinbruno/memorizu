913c9d9226dfaebf0ce53802f6eeebbe37d52cc9
dinbruno <brunodinosantos@Outlook.com>
2025-05-29 18:35:49 -0300

    Update Firebase redirects in next.config.mjs to point to the new domain
    - Changed Firebase Auth and function redirect destinations from the old Firebase domain to the new memorizu.com domain for improved consistency and user experience.

--- next.config.mjs
+++ next.config.mjs
@@ -14,14 +14,14 @@ const nextConfig = {
       // Redirect Firebase Auth handler to Firebase domain
       {
         source: "/__/auth/:path*",
-        destination: "https://memorizu-7bd4c.firebaseapp.com/__/auth/:path*",
+        destination: "https://memorizu.com/__/auth/:path*",
         permanent: false,
         basePath: false,
       },
       // Redirect Firebase functions
       {
         source: "/__/:path*",
-        destination: "https://memorizu-7bd4c.firebaseapp.com/__/:path*",
+        destination: "https://memorizu.com/__/:path*",
         permanent: false,
         basePath: false,
       },
