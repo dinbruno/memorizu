0457fc5a6d880631e518cc49ca7d4dd6f97ead89
dinbruno <brunodinosantos@Outlook.com>
2025-05-29 10:32:22 -0300

    Add redirects for Firebase authentication and functions in next.config.mjs
    - Implemented redirects for Firebase Auth handler and functions to route requests to the Firebase domain.
    - Enhanced configuration to support seamless integration with Firebase services, improving user experience during authentication and function calls.

--- next.config.mjs
+++ next.config.mjs
@@ -19,6 +19,24 @@ const nextConfig = {
       },
     ];
   },
+  async redirects() {
+    return [
+      // Redirect Firebase Auth handler to Firebase domain
+      {
+        source: "/__/auth/:path*",
+        destination: "https://memorizu-7bd4c.firebaseapp.com/__/auth/:path*",
+        permanent: false,
+        basePath: false,
+      },
+      // Redirect Firebase functions
+      {
+        source: "/__/:path*",
+        destination: "https://memorizu-7bd4c.firebaseapp.com/__/:path*",
+        permanent: false,
+        basePath: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
